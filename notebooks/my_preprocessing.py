404cfc507440ef5e17f1d32ffdeb6e4a48f2ba40
Kurt Eulau <kurteulau@gmail.com>
2022-03-03 21:53:37 +0000

    Attempt to split on train, val, test and run model

--- notebooks/my_preprocessing.py
+++ notebooks/my_preprocessing.py
@@ -41,3 +41,46 @@ def train_dev_test_split(df, column='location', pct_train=0.6, pct_dev=0.2, pct_
         test = pd.concat(objs=[test, col_df_test])
 
     return train, dev, test
+
+
+# Exactly the same as above but uses validation/val instead of development/dev nomenclature
+def train_val_test_split(df, column='location', pct_train=0.6, pct_val=0.2, pct_test=0.2, random_state=42):
+    """
+    Splits up a dataset using stratified random sampling to ensure equal proportion of observations by column
+    in each of the train, development, and test sets.
+    
+    Params: 
+     - df (DataFrame): pandas dataframe you want to split into train
+     - col (str): column you want to stratify on. Default value is 'location'
+     - pct_train (float): percent of dataset you want in the training set reprsented as float between 0 and 1. Default value is 0.6
+     - pct_val (float): percent of the dataset you want in the validation set reprsented as float between 0 and 1. Default value is 0.2
+     - pct_test (float): percent of the dataset you want in the test set reprsented as float between 0 and 1. Default value is 0.2
+    Returns:
+    - tuple with train, dev, and test datasets as pandas dataframes
+    """
+    
+
+    train = pd.DataFrame()
+    val = pd.DataFrame()
+    test = pd.DataFrame()
+
+    for value in df[column].unique():
+        # Create a dataframe for that column value and shuffle it
+        col_df = df[df[column] == value]
+        col_df_shuffled = shuffle(col_df, random_state=random_state)
+
+        # Create splits for train, val, and test sets
+        split_1 = int(pct_train * col_df.shape[0])
+        split_2 = int((pct_train + pct_val) * col_df.shape[0])
+
+        # Split up shuffled dataframe (for each col)
+        col_df_train = col_df_shuffled.iloc[:split_1]
+        col_df_val = col_df_shuffled.iloc[split_1:split_2]
+        col_df_test = col_df_shuffled.iloc[split_2:]
+
+        # Add on the selections for train, val, and test
+        train = pd.concat(objs=[train, col_df_train])
+        val = pd.concat(objs=[val, col_df_val])
+        test = pd.concat(objs=[test, col_df_test])
+
+    return train, val, test
